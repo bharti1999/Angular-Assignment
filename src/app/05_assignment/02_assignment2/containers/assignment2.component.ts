432a6e438894bcf3f6ba8244bf9ae6a826887e5a
Bharti Sharma <bharti.sharma@nagarro.com>
2020-04-22 15:56:48 +0530

    submission

---
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from '../../assignment.component';
+
+@Component({
+  selector: 'app-assignment2',
+  templateUrl: './assignment2.component.html',
+  styleUrls: ['./assignment2.component.css']
+})
+export class Assignment2Component {
+  @Input()
+  todos: Todo[];
+
+  selectedTodo: Todo;
+
+  onClick(todo: Todo){
+    this.selectedTodo = todo;
+  }
+}
